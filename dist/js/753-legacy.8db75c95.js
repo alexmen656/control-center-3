a6a8ad4171acabc948a0c67dd52f7ac2a21a7fdd
Alex Polan <alex@aglo.eu>
2023-07-30 17:39:56 +0200

    Update version to 0.4.0-alpha and delete unnecessary files

--- dist/js/753-legacy.8db75c95.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkcontrol_center"]=self["webpackChunkcontrol_center"]||[]).push([[753],{4753:(n,e,t)=>{t.r(e),t.d(e,{mdTransitionAnimation:()=>a});var o=t(5980),i=t(5026);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const a=(n,e)=>{var t,a,r;const l="40px",s="0px",c="back"===e.direction,d=e.enteringEl,u=e.leavingEl,m=(0,i.g)(d),b=m.querySelector("ion-toolbar"),f=(0,o.c)();if(f.addElement(m).fill("both").beforeRemoveClass("ion-page-invisible"),c?f.duration((null!==(t=e.duration)&&void 0!==t?t:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)"):f.duration((null!==(a=e.duration)&&void 0!==a?a:0)||280).easing("cubic-bezier(0.36,0.66,0.04,1)").fromTo("transform",`translateY(${l})`,`translateY(${s})`).fromTo("opacity",.01,1),b){const n=(0,o.c)();n.addElement(b),f.addAnimation(n)}if(u&&c){f.duration((null!==(r=e.duration)&&void 0!==r?r:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)");const n=(0,o.c)();n.addElement((0,i.g)(u)).onFinish((e=>{1===e&&n.elements.length>0&&n.elements[0].style.setProperty("display","none")})).fromTo("transform",`translateY(${s})`,`translateY(${l})`).fromTo("opacity",1,0),f.addAnimation(n)}return f}}}]);
-//# sourceMappingURL=753-legacy.8db75c95.js.map
--- dist/js/78.3b323ab0.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkcontrol_center"]=self["webpackChunkcontrol_center"]||[]).push([[78],{6078:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>c});var r=n(65),o=n(4074),s=n(6587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const c=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,c=document.elementFromPoint(t/2,n/2);if(!c)return;const a=(0,o.a)(c);a&&new Promise((e=>(0,s.c)(a,e))).then((()=>{(0,r.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,o.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.3b323ab0.js.map
