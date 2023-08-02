75338ec0d72405f88196f0839d34818194328e83
Alex Polan <alex@aglo.eu>
2023-08-02 14:09:27 +0200

    feat: Add component creation and change logging

--- backend/components.php
+++ backend/components.php
@@ -2,131 +2,136 @@
 include 'head.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+$headers = getRequestHeaders();
+
+if ($headers['Authorization']) {
+  $token = escape_string($headers['Authorization']);
+  $userData = query("SELECT * FROM control_center_users WHERE loginToken='$token'");
+
+  if (mysqli_num_rows($userData) == 1) {
+    $userData = fetch_assoc($userData);
+    $userID = $userData['userID'];
+
+    if (isset($_POST['getComponentsByProject']) && isset($_POST['project'])) {
+      $projectName = escape_string($_POST['project']);
+      $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
+
+      $components = query("SELECT * FROM project_components WHERE projectID='$projectID' ORDER BY `project_components`.`id` ASC");
+      $i = 0;
+      foreach ($components as $c) {
+        $json[$i]['name'] = $c['name'];
+        $json[$i]['code'] = $c['code'];
+        $i++;
+      }
+      echo echoJSON($json);
+    } elseif (isset($_POST['getCoponent']) && isset($_POST['name']) && isset($_POST['project'])) {
+
+      $projectName = escape_string($_POST['project']);
+      $name = escape_string($_POST['name']);
+      $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
+      $components = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name' ORDER BY `project_components`.`id` ASC");
+      foreach ($components as $c) {
+        if ($c['type'] == "script") {
+          $content = file_get_contents("/www/" . $projectName . "/" . $c['file']);
+        } elseif ($c['type'] == "audio" || $c['type'] == "video" || $c['type'] == "image") {
+          $content = $projectName . "/" . $c['file'];
+        }
+        $last_change_by = $c['last_change_by'];
+        $query = query("SELECT * FROM control_center_users WHERE userID='$last_change_by'");
+        if (mysqli_num_rows($query) == 1) {
+          $json['last_change'] = $c['last_change'];
+          $uD = fetch_assoc($query);
+          $json['last_change_by'] = $uD['firstname'] . " " . $uD['lastname'];
+        }
+
+      }
+      $json['createdOn'] = $c['createdOn'];
+      $json['name'] = $c['name'];
+      $json['type'] = $c['type'];
+      $json['content'] = $content;
+      echo echoJSON($json);
+    } elseif (isset($_POST['deleteComponent']) && isset($_POST['name']) && isset($_POST['project'])) {
 
 
-if (isset($_POST['getComponentsByProject']) && isset($_POST['project'])) {
-  //echo 1;
-  $projectName = escape_string($_POST['project']);
-  //echo $projectName;
-  $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
-  //  echo $projectID
-  $components = query("SELECT * FROM project_components WHERE projectID='$projectID' ORDER BY `project_components`.`id` ASC");
-  $i = 0;
-  foreach ($components as $c) {
-    $json[$i]['name'] = $c['name'];
-    $json[$i]['code'] = $c['code'];
-    $i++;
-  }
-  echo echoJSON($json);
-} elseif (isset($_POST['getCoponent']) && isset($_POST['name']) && isset($_POST['project'])) {
-  // echo 1;
-  $projectName = escape_string($_POST['project']);
-  $name = escape_string($_POST['name']);
-  //echo $projectName;
-  $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
-  //  echo $projectID." ".$name;
-  $components = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name' ORDER BY `project_components`.`id` ASC");
-  foreach ($components as $c) {
-    if ($c['type'] == "script") {
-      $content = file_get_contents("/www/" . $projectName . "/" . $c['file']);
-    } elseif ($c['type'] == "audio" || $c['type'] == "video" || $c['type'] == "image") {
-      $content = $projectName . "/" . $c['file'];
-    }
+      $projectName = escape_string($_POST['project']);
+      $name = escape_string($_POST['name']);
+      $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
+      $component = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name' ORDER BY `project_components`.`id` ASC");
 
+      if (mysqli_num_rows($component) != 1) {
+        echo "error 1";
+      } else {
+        $component = fetch_assoc($component);
+        $delete = query("DELETE FROM project_components WHERE projectID='$projectID' AND code='$name'");
+        if ($delete) {
+          if (unlink("/www/" . $projectName . "/" . $component['file'])) {
+            echo "component/s successful deleted";
+          }
+        }
+      }
 
-    $json['name'] = $c['name'];
-    $json['type'] = $c['type'];
-    $json['content'] = $content;
-  }
-  echo echoJSON($json);
-} elseif (isset($_POST['deleteComponent']) && isset($_POST['name']) && isset($_POST['project'])) {
+    } elseif (isset($_POST['updateHTML']) && isset($_POST['name']) && isset($_POST['project']) && isset($_POST['html'])) {
+      $projectName = escape_string($_POST['project']);
+      $name = escape_string($_POST['name']);
+      $html = $_POST['html'];
 
+      $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
+      $component = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name'");
 
-  $projectName = escape_string($_POST['project']);
-  $name = escape_string($_POST['name']);
-  $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
-  $component = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name' ORDER BY `project_components`.`id` ASC");
+      if (mysqli_num_rows($component) == 1) {
+        $filename = fetch_assoc($component)['file'];
+        $location = "/www/" . $projectName . "/" . $filename;
+        $oldHTML = file_get_contents($location);
 
-  if (mysqli_num_rows($component) != 1) {
-    echo "error 1";
-  } else {
-    $component = fetch_assoc($component);
-    $delete = query("DELETE FROM project_components WHERE projectID='$projectID' AND code='$name'");
-    if ($delete) {
-      if (unlink("/www/" . $projectName . "/" . $component['file'])) {
-        echo "component/s successful deleted";
+        if ($oldHTML != $html) {
+          unlink($location);
+          file_put_contents($location, $html, 0777);
+          chmod($location, 0777);
+
+          if (query("UPDATE project_components SET last_change=NOW(), last_change_by='$userID' WHERE projectID='$projectID' AND code='$name'")) { //last_change=NOW() AND 
+            echo "Success";
+          }
+        } else {
+          echo "0 change!";
+        }
+
+      } else {
+        echo "error 1";
+      }
+
+    } elseif (isset($_POST['newComponent']) && isset($_POST['name']) && isset($_POST['code']) && isset($_POST['project'])) { // && isset($_POST['html']))
+      $projectName = escape_string($_POST['project']);
+      $name = escape_string($_POST['name']);
+      $code = escape_string($_POST['code']);
+      $type = "script";
+      $icon = "code-slash-outline";
+
+      if (isset($_FILES["files"])) {
+        $type = "image";
+        $icon = "image-outline";
+        foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+          $name2 = $_FILES['files']['name'][$key];
+          $file_info = pathinfo($name2, PATHINFO_EXTENSION);
+          $fileName = $code . "." . $file_info;
+          $file_destination = '/www/' . $projectName . '/' . $fileName;
+          move_uploaded_file($tmp_name, $file_destination);
+        }
+
+      } else {
+        $fileName = str_replace(" ", "-", strtolower($code)) . ".php";
+      }
+
+      $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
+      $insert = query("INSERT INTO project_components VALUES(0, '$fileName', '$type', '$name', '$code', NOW(), NOW(), '$userID', '1234567890', '$projectID')");
+      $url = "project/" . $projectName . "/components/" . $code;
+      query("INSERT INTO control_center_pages VALUES (0, '$url', 'true', '$icon', '$name', '', 0)");
+      if ($insert) {
+        $location = "/www/" . $projectName . "/" . $fileName;
+        file_put_contents($location, "", 0777);
+        chmod($location, 0777);
+      } else {
+        echo "error 1";
       }
     }
   }
-
-
-} elseif (isset($_POST['updateHTML']) && isset($_POST['name']) && isset($_POST['project']) && isset($_POST['html'])) {
-  // echo 1;
-  $projectName = escape_string($_POST['project']);
-  $name = escape_string($_POST['name']);
-  $html = $_POST['html']; //escape_string(
-  echo 1;
-  //echo $projectName;
-  $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
-  //  echo $projectID." ".$name;
-  $component = query("SELECT * FROM project_components WHERE projectID='$projectID' AND code='$name'");
-  if (mysqli_num_rows($component) == 1) {
-    echo 2;
-
-    $filename = fetch_assoc($component)['file'];
-    $location = "/www/" . $projectName . "/" . $filename;
-    echo $location;
-    echo file_get_contents($location);
-    echo $html . " <== Hier";
-    unlink($location);
-    //chmod($location, 0777);
-    file_put_contents($location, $html, 0777);
-    chmod($location, 0777);
-
-  } else {
-    echo "error 1";
-  }
-
-  //echo echoJSON($json);
-} elseif (isset($_POST['newComponent']) && isset($_POST['name']) && isset($_POST['code']) && isset($_POST['project'])) { // && isset($_POST['html']))
-  //echo 2;
-  $projectName = escape_string($_POST['project']);
-  $name = escape_string($_POST['name']);
-  $code = escape_string($_POST['code']);
-  $type = "script";
-  $icon = "code-slash-outline";
-
-  if (isset($_FILES["files"])) {
-    $type = "image";
-    $icon = "image-outline";
-    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
-      $name2 = $_FILES['files']['name'][$key];
-      $file_info = pathinfo($name2, PATHINFO_EXTENSION);
-      $fileName = $code . "." . $file_info;
-
-      $file_destination = '/www/' . $projectName . '/' . $fileName;
-      move_uploaded_file($tmp_name, $file_destination);
-      echo $fileName;
-    }
-
-  } else {
-    $fileName = str_replace(" ", "-", strtolower($code)) . ".php";
-  }
-
-
-  //$html = $_POST['html'];//escape_string(
-
-
-  $projectID = fetch_assoc(query("SELECT * FROM projects WHERE link='$projectName'"))['projectID'];
-  $insert = query("INSERT INTO project_components VALUES(0, '$fileName', '$type', '$name', '$code', '1234567890', '$projectID')");
-  $url = "project/" . $projectName . "/components/" . $code;
-  query("INSERT INTO control_center_pages VALUES (0, '$url', 'true', '$icon', '$name', '', 0)");
-  if ($insert) {
-    $location = "/www/" . $projectName . "/" . $fileName;
-    file_put_contents($location, "", 0777);
-    chmod($location, 0777);
-  } else {
-    echo "error 1";
-  }
-
 }
--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -4,10 +4,10 @@
       <ion-col size="1" />
       <ion-col
         size="10"
-        style="display: flex; content-align: center; flex-direction: column"
+        style="display: flex; align-items: center; flex-direction: column"
       >
         <code-editor
-          v-if="type == 'script'"
+          v-if="type === 'script'"
           :wrap_code="false"
           style="overflow-y: scroll !important"
           v-model="newHTML"
@@ -20,15 +20,24 @@
             ['html', 'HTML'],
           ]"
         />
-        <ion-button v-if="type == 'script'" @click="saveHTML()" color="danger"
+        <ion-button v-if="type === 'script'" @click="saveHTML()" color="primary"
           >Save Component</ion-button
         >
-        <img v-if="type == 'image'" :src="'https://alex.polan.sk/' + newHTML" alt="" />
+        <img
+          v-if="type === 'image'"
+          :src="'https://alex.polan.sk/' + newHTML"
+          alt="Image"
+        />
+        <span v-if="dateTimeString && component.last_change_by"
+          >Last Change: {{ dateTimeString }} by
+          {{ component.last_change_by }}</span
+        ><span v-if="createdOn">Created: {{ createdOn }}</span>
       </ion-col>
       <ion-col size="1" />
     </ion-row>
   </ion-grid>
 </template>
+
 <script>
 import axios from "axios";
 import qs from "qs";
@@ -50,38 +59,36 @@ export default {
       component: {},
       newHTML: "",
       type: "",
+      dateTimeString: "",
+      createdOn: "",
     };
   },
   async mounted() {
-    const route = useRoute();
-    const component = {};
-
-    await axios
-      .post(
-        "/control-center/components.php",
-        qs.stringify({
-          getCoponent: "getCoponent",
-          project: route.params.project,
-          name: route.params.component,
-        })
-      )
-      .then((res) => {
-        component.content = res.data.content;
-        component.type = res.data.type;
-        //console.log(component.content);
-      });
-
-    this.component = component;
-    this.newHTML = component.content.replaceAll("\n", "\n");
-    this.type = component.type;
-    //this.newHTML = "hello\n\nhello\n\n"
-    //console.log(this.type);
+    await this.getComponentData();
   },
   methods: {
-    saveHTML() {
-      const route = useRoute();
+    async getComponentData() {
+      try {
+        const response = await axios.post(
+          "/control-center/components.php",
+          qs.stringify({
+            getCoponent: "getCoponent",
+            project: this.$route.params.project,
+            name: this.$route.params.component,
+          })
+        );
 
-      console.log(this.newHTML);
+        this.component = response.data;
+        this.newHTML = this.component.content.trim();
+        this.type = this.component.type;
+
+        this.updateDateTimeString();
+      } catch (error) {
+        console.error("Error fetching component data:", error);
+      }
+    },
+    saveHTML() {
+      const html = this.newHTML.trim();
       axios
         .post(
           "/control-center/components.php",
@@ -89,7 +96,7 @@ export default {
             updateHTML: "updateHTML",
             project: this.$route.params.project,
             name: this.$route.params.component,
-            html: this.newHTML.replaceAll("\n", "\n").trim(),
+            html: html,
           }),
           {
             headers: {
@@ -97,15 +104,40 @@ export default {
             },
           }
         )
-        .then((res) => {
-          //replaceAll("\n", "\r\n")
-          //component.content = res.data.content;
-          //console.log(component.content);
+        .then(() => {
+          this.getComponentData();
+        })
+        .catch((error) => {
+          console.error("Error saving component HTML:", error);
         });
+    },
+    updateDateTimeString() {
+      if (new Date(this.component.last_change) != "Invalid Date") {
+        const lastChangeDate = new Date(this.component.last_change);
+        const formattedDate = lastChangeDate
+          .toLocaleDateString("en-GB")
+          .split("/")
+          .join(".");
+        const formattedTime = lastChangeDate.toLocaleTimeString("en-GB");
+        this.dateTimeString = formattedDate + " at " + formattedTime;
+      }
+      if (new Date(this.component.createdOn) != "Invalid Date") {
+        const createdOn = new Date(this.component.createdOn);
+        const formattedCreatedOnDate = createdOn
+          .toLocaleDateString("en-GB")
+          .split("/")
+          .join(".");
+        const formattedCreatedOnTime = createdOn.toLocaleTimeString("en-GB");
+        this.createdOn =
+          formattedCreatedOnDate + " at " + formattedCreatedOnTime;
+      } else {
+        this.createdOn = "Not Avaible";
+      }
     },
   },
 };
 </script>
+
 <style>
 textarea {
   overflow-y: scroll !important;
