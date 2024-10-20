b9d1d5143bdad0b2581d641c28839476a61f30c3
Alex Polan <alex@aglo.eu>
2024-10-20 15:41:18 +0200

    chore: Move configuration variables to .env file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,28 +1,27 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "sk.polan.alex.control-center",
-  appName: "Control Center",
-  webDir: "dist",
+  appId: process.env.APP_ID || "sk.polan.alex.control-center",
+  appName: process.env.APP_NAME || "Control Center",
+  webDir: process.env.WEB_DIR || "dist",
   bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: "#000000",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
+      launchShowDuration: parseInt(process.env.SPLASH_LAUNCH_SHOW_DURATION) || 2000,
+      launchAutoHide: process.env.SPLASH_LAUNCH_AUTO_HIDE === "true",
+      backgroundColor: process.env.SPLASH_BACKGROUND_COLOR || "#000000",
+      androidSplashResourceName: process.env.ANDROID_SPLASH_RESOURCE_NAME || "splash",
+      androidScaleType: process.env.ANDROID_SCALE_TYPE || "CENTER_CROP",
+      splashFullScreen: process.env.SPLASH_FULL_SCREEN === "true",
+      splashImmersive: process.env.SPLASH_IMMERSIVE === "true",
+      layoutName: process.env.LAYOUT_NAME || "launch_screen",
+      useDialog: process.env.USE_DIALOG === "true",
     },
     GoogleAuth: {
       scopes: ["profile", "email"],
-      serverClientId:
-        "706582238302-k3e6bqv81en6u97gf8l5pq883p773236.apps.googleusercontent.com",
+      serverClientId: process.env.GA_SCID,
       forceCodeForRefreshToken: true,
-      iosClientId: "706582238302-q1i78up9i9p5ia3tmaurqeerll1ucthg.apps.googleusercontent.com"
+      iosClientId: process.env.GA_ICID
     },
     FirebaseMessaging: {
       presentationOptions: ["badge", "sound", "alert"]
--- src/firebase_config.ts
+++ src/firebase_config.ts
@@ -1,9 +1,9 @@
 export const firebase_config = {
-    apiKey: "AIzaSyC8pyumD9Xi5jp5XBzijbaVtI16mIQKsdY",
-    authDomain: "control-center-2.firebaseapp.com",
-    projectId: "control-center-2",
-    storageBucket: "control-center-2.appspot.com",
-    messagingSenderId: "706582238302",
-    appId: "1:706582238302:web:f55a89f5e66e298eb490cf",
-    vapidKey: "BPiHnz-4ouo7GvDhi8TzNR_VoCAl1P54f-wV55_WKeLUbrJDfdlwcIZC6GukgNXcGi_rToo5Yn9E-7gEgjV7Izc"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    vapidKey: process.env.FIREBASE_VAPID_KEY
   };
