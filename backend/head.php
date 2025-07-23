b48df9d98af374975d5880591f21671c1b07b685
Alex Polan <alexpolan@icloud.com>
2025-07-23 20:59:43 +0200

    feat: Update head.php to verify JWTs

--- backend/head.php
+++ backend/head.php
@@ -1,12 +1,15 @@
 <?php
+include_once 'jwt_helper.php';
+include_once 'config.php';
+
 $origin_url = $_SERVER['HTTP_ORIGIN'] ?? $_SERVER['HTTP_REFERER'];
 $allowed_origins = ['alexsblog.de', 'localhost:8100', 'polan.sk', 'http://localhost:8100/login', 'http://localhost:8100', 'localhost']; // replace with query for domains.
 $request_host = parse_url($origin_url, PHP_URL_HOST);
 $host_domain = implode('.', array_slice(explode('.', $request_host), -2));
 //echo $host_domain;
 //if (! in_array($host_domain, $allowed_origins, false)) {
-  //  header('HTTP/1.0 403 Forbidden');
-  //  die('You are not allowed to access this.');     
+//  header('HTTP/1.0 403 Forbidden');
+//  die('You are not allowed to access this.');     
 //}
 session_start();
 header('Access-Control-Allow-Origin: *');
@@ -14,6 +17,22 @@ header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Content-Type: application/json');
 include '/www/paxar/components/php_head.php';
+
+// JWT prüfen
+$headers = function_exists('getallheaders') ? getallheaders() : [];
+if (isset($headers['Authorization'])) {
+    $token = $headers['Authorization'];
+    $payload = SimpleJWT::verify($token, $jwt_secret);
+    if (!$payload) {
+        header('HTTP/1.1 401 Unauthorized');
+        echo json_encode(['error' => 'No valid token']);
+        exit;
+    }
+} else {
+    header('HTTP/1.1 401 Unauthorized');
+    echo json_encode(['error' => 'No valid token']);
+    exit;
+}
 
 function randomNumber(){
     $rand = rand(100000, 999999);
