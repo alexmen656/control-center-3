8a2a5458b0241408584d609efd6a3b00672a0485
Alex <133679349+alexmen656@users.noreply.github.com>
2026-01-10 18:09:42 +0100

    refactor: Clean up Modal.vue and DynamicModal.vue

--- web-builder/src/Components/Modals/Modal.vue
+++ web-builder/src/Components/Modals/Modal.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { computed, onMounted, onUnmounted, watch } from "vue";
+import { computed } from "vue";
 import {
     Dialog,
     DialogOverlay,
-    DialogTitle,
     TransitionChild,
     TransitionRoot,
 } from "@headlessui/vue";
@@ -54,50 +53,23 @@ const maxWidthClass = computed(() => {
 <template>
     <teleport to="body">
         <TransitionRoot :show="show" as="template">
-            <Dialog
-                as="div"
-                class="fixed z-30 inset-0 overflow-y-auto"
-                @close="close"
-            >
-                <div
-                    class="flex items-end justify-center text-center sm:block sm:p-0"
-                >
-                    <TransitionChild
-                        as="template"
-                        enter="ease-out duration-100"
-                        enter-from="opacity-0"
-                        enter-to="opacity-100"
-                        leave="ease-in duration-100"
-                        leave-from="opacity-100"
-                        leave-to="opacity-0"
-                    >
-                        <DialogOverlay
-                            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-                        />
+            <Dialog as="div" class="fixed z-30 inset-0 overflow-y-auto" @close="close">
+                <div class="flex items-end justify-center text-center sm:block sm:p-0">
+                    <TransitionChild as="template" enter="ease-out duration-100" enter-from="opacity-0"
+                        enter-to="opacity-100" leave="ease-in duration-100" leave-from="opacity-100"
+                        leave-to="opacity-0">
+                        <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                     </TransitionChild>
-                    <!-- This element is to trick the browser into centering the modal contents. -->
-                    <span
-                        aria-hidden="true"
-                        class="hidden sm:inline-block sm:align-middle sm:h-screen"
-                        >&#8203;</span
-                    >
-                    <TransitionChild
-                        as="template"
-                        enter="ease-out duration-100"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="ease-in duration-100"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <div
-                            class="relative inline-block align-bottom bg-white rounded-xl text-left overflow-hidden shadow-xl transform transition-all sm:align-top top-4 sm:w-full w-[96%]"
+                    <span aria-hidden="true" class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+                    <TransitionChild as="template" enter="ease-out duration-100" enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100" leave="ease-in duration-100" leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95">
+                        <div class="relative inline-block align-bottom bg-white rounded-xl text-left overflow-hidden shadow-xl transform transition-all sm:align-top top-4 sm:w-full w-[96%]"
                             :class="[
                                 maxWidthClass ? maxWidthClass : '',
                                 minHeight ? minHeight : '',
                                 maxHeight ? maxHeight : '',
-                            ]"
-                        >
+                            ]">
                             <slot></slot>
                         </div>
                     </TransitionChild>
