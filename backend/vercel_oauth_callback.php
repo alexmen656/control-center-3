5b0001a8dbe2bc39c953f6aabae3e16c02d74c14
Alex <133679349+alexmen656@users.noreply.github.com>
2026-07-08 10:06:49 +0200

    chore: Remove unused files

--- backend/vercel_oauth_callback.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-
-$frontend_url_dev = 'http://localhost:5173/my-account/account-security';
-$frontend_url_prod = 'https://app.fringelo.com/my-account/account-security';
-$frontend_url = (defined('APP_ENV') && APP_ENV === 'dev') ? $frontend_url_dev : $frontend_url_prod;
-
-header('Location: ' . $frontend_url . '?vercel_disabled=1');
-exit;
